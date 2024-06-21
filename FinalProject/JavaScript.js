70a1f6b6957b81ddacff216169a30e646dc288c7
Mason Strewons <mstrewons@gmail.com>
2024-06-21 02:24:19 -0700

    Add project files.

---
+++ FinalProject/JavaScript.js
@@ -0,0 +1,16 @@
+﻿function projectAnimation() {
+    let x = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(x);
+    id = setInterval(frame, 20);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(x);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
